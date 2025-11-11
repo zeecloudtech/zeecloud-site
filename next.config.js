ba1e495d478352d9a519f410e2c5f97b66196671
zeecloudtech <zegseifu@gmail.com>
2025-11-11 15:11:53 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // enables static HTML export for Azure Static Web Apps
+  output: 'export',
   reactStrictMode: true,
   images: {
-    unoptimized: true, // needed for static hosting
-  },
-  // optional: if you had public runtime vars, move them here
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || '',
+    unoptimized: true,
   },
 };
 
